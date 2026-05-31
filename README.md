4c02d56f07aa964830b37278004b397ff424b12d
Ivan3952 <109902324+Ivan3952@users.noreply.github.com>
2026-05-31 03:39:01 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+# LOTOS Event Site Demo
+
+Новый сайт на базе COLUMBINA, но в стиле Imperial Space Lotus.
+
+Есть:
+- главная LOTOS;
+- intro;
+- победители с пагинацией;
+- все работы;
+- режим галереи;
+- случайная работа;
+- музыка только по кнопке;
+- комментарии к работам;
+- lightbox с комментарием.
+
+Формат data.js:
+{
+  id: 1,
+  username: "@user",
+  title: "Работа #1",
+  image: "img/works/001_user.webp",
+  postLink: "https://...",
+  score: 9,
+  reward: 1200,
+  isWinner: true,
+  place: 2,
+  awardTitle: "Golden Lotus",
+  comment: "Комментарий к работе."
+}
---
+++ data.js
@@ -0,0 +1,7 @@
+const EVENT = { title: "LOTOS", subtitle: "Итоги ивента", cover: "img/cover.svg", stats: "" };
+
+const WORKS = [
+  { id: 1, username: "@jade_lotus", title: "Победная работа", image: "img/works/placeholder.svg", postLink: "", score: 10, reward: 2100, isWinner: true, place: 1, awardTitle: "Imperial Lotus", comment: "Очень сильное попадание в тему: работа выглядит как фрагмент императорского космического архива. Композиция читается сразу, цвета собраны аккуратно, а образ не разваливается на детали." },
+  { id: 2, username: "@red_palace", title: "Работа участника", image: "img/works/placeholder.svg", postLink: "", score: 8.5, reward: 0, isWinner: false, place: null, awardTitle: "", comment: "Хорошая атмосфера и приятная палитра. Можно было бы сильнее выделить главный объект, но общая подача работает." },
+  { id: 3, username: "@star_sakura", title: "Special Mention", image: "img/works/placeholder.svg", postLink: "", score: 9, reward: 500, isWinner: true, place: 4, awardTitle: "Jade Mention", comment: "Красивая работа с мягким настроением. Особенно удачно смотрится сочетание света и декоративных элементов." }
+];
