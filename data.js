a372e866ee23a819234fa805fbdb7692328c89b7
Ivan3952 <109902324+Ivan3952@users.noreply.github.com>
2026-06-04 21:10:08 +0300

    Update data.js

--- data.js
+++ data.js
@@ -8,21 +8,21 @@ const EVENT = {
 const WORKS = [
   {
     id: 1,
-    username: "@jade_lotus",
-    title: "Победная работа",
+    username: "test",
+    title: "test",
     image: "img/works/placeholder.svg",
     postLink: "",
     score: 10,
     reward: 2100,
     isWinner: true,
     place: 1,
-    awardTitle: "Imperial Lotus",
-    comment: "Очень сильное попадание в тему: работа выглядит как фрагмент императорского космического архива. Композиция читается сразу, цвета собраны аккуратно, а образ не разваливается на детали."
+    awardTitle: "test",
+    comment: "test."
   },
   {
     id: 2,
-    username: "@red_palace",
-    title: "Работа участника",
+    username: "test",
+    title: "test",
     image: "img/works/placeholder.svg",
     postLink: "",
     score: 8.5,
@@ -30,19 +30,19 @@ const WORKS = [
     isWinner: false,
     place: null,
     awardTitle: "",
-    comment: "Хорошая атмосфера и приятная палитра. Можно было бы сильнее выделить главный объект, но общая подача работает."
+    comment: "test"
   },
   {
     id: 3,
-    username: "@star_sakura",
-    title: "Special Mention",
+    username: "test",
+    title: "test",
     image: "img/works/placeholder.svg",
     postLink: "",
     score: 9,
     reward: 500,
     isWinner: true,
     place: 4,
-    awardTitle: "Jade Mention",
-    comment: "Красивая работа с мягким настроением. Особенно удачно смотрится сочетание света и декоративных элементов."
+    awardTitle: "test",
+    comment: "test"
   }
 ];
